6f6f64ca84f07beb4379d9da023742672a9c8cbe
horaciodemaio <horaciodemaio33@gmail.com>
2021-08-03 18:50:36 -0300

    api/ arregle nombre a la tabla de bodegas

--- api/src/Models/WineCellar.js
+++ api/src/Models/WineCellar.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    sequelize.define("variety", {
+    sequelize.define("wineCellar", {
         name: {
             type: DataTypes.STRING,
             allowNull: false,
